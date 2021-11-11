fcffd24dbf2639560100fb1893c0f3ffd828d284
Maya-Israeli <maya.i@minutemedia.com>
2021-11-11 10:04:21 +0200

    Clean Code JSON

--- server.js
+++ server.js
@@ -8,44 +8,21 @@ console.log(__filename, module, global)
 http.createServer(function (req, res) {
     let fileName = req.url;
     let end = checkEnd(fileName);
-    switch (end) {
-        case 'html': { 
-            fs.readFile('client/flights.html', function(err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            }); 
-            break;
-        } 
-        case 'css': { 
-          
-            break;
-        } 
-        case 'js': { 
-            
-            break;
-        } 
-        case 'json': { 
-            console.log('in json');
-            fs.readFile('client/data.json', function(err, data) {
-                const flights = JSON.parse(data);
-                console.log(flights);
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify(flights));
-                res.end();
-            }); 
-            break;
-        } 
-        default: {
-            fs.readFile('client/flights.html', function(err, data) {
-                console.log('end: ',end);
-                console.log('in html');
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            }); 
-            break;
-        }
+    let contentType = readUrl(end);
+    
+    if (end === 'json') {
+        fs.readFile('client/data.json', function(err, data) {
+            const flights = JSON.parse(data);
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(JSON.stringify(flights));
+            res.end();
+        })
+    } else{
+        fs.readFile('client/flights.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(data);
+            res.end();
+        })
     }
 }).listen(8080, function () {
     console.log('Client is available at http://localhost:8080');
@@ -55,3 +32,19 @@ function checkEnd(fileName){
     let index = fileName.lastIndexOf('.');
     return fileName.substr(index+1);
 }
+
+function readUrl(end){
+    switch(end){
+        case 'json': 
+            return 'application/json';
+        case 'html': 
+            return 'text/html';
+        case 'css': 
+            return 'test/css';
+        case 'js': 
+            return 'text/javascript';
+        default:
+            return 'text/html';
+    } 
+}
+    
